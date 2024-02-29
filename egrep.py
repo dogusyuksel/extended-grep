8d7cb7c6fa78e9242978d609bb54d2721fd11ce0
Dogus Yuksel <dogusyuk@gmail.com>
2024-02-29 15:16:26 +0000

    edited source code with respect to flake8 and pytest infrastructure added

--- egrep.py
+++ egrep.py
@@ -1,14 +1,16 @@
-import sys
 import argparse
+import sys
+
 import matplotlib.pyplot as plt
 
-VERSION="0.0.1"
+import egrep_utils
+
+VERSION = "0.0.1"
 
 parser = argparse.ArgumentParser(
-                    prog='ProgramName',
-                    description='What the program does',
-                    epilog='Text at the bottom of help')
-
+    prog='egrep',
+    description='an extended version of \'grep\'',
+    epilog='egrep Help')
 parser.add_argument('-v', '--version',
                     action='store_true',
                     help='show version number and return')
@@ -18,16 +20,16 @@ parser.add_argument('-d', '--verbose',
 parser.add_argument('-f', '--filename',
                     action='store',
                     help='direct path to the log file')
-################################################## FINDING LINE ##########################################################
+# FINDING LINE
 parser.add_argument('-s', '--startline',
                     action='store',
-                    help='starting line to run the query while line numbering ' \
-                        'starts from 1 and the input numbered line will be included',
+                    help='starting line to run the query while line numbering '
+                    'starts from 1 and the input numbered line will be included',
                     type=int)
 parser.add_argument('-e', '--endline',
                     action='store',
-                    help='ending line to run the query while the input numbered ' \
-                        'line will be included',
+                    help='ending line to run the query while the input numbered '
+                    'line will be included',
                     type=int)
 parser.add_argument('-l', '--showlineno',
                     action='store_true',
@@ -42,20 +44,22 @@ parser.add_argument('-b', '--linebelow',
                     action='store',
                     help='select the number of lines below of the keywords containing line',
                     type=int)
-################################################## FINDING LINE ENDS ##########################################################
-################################################## FINDING WORDS ##########################################################
+# FINDING LINE ENDS
+# FINDING WORDS
 parser.add_argument('-p', '--seperator',
                     action='store',
-                    help='selected line will be splitted into words around this character, default is SPACE')
+                    help='selected line will be splitted into words around this character, '
+                    'default is SPACE')
 parser.add_argument('-m', '--elementat',
                     action='store',
-                    help='the number of i\'th element in the line after splitting with respect to seperator, starting from 0',
+                    help='the number of i\'th element in the line after splitting with respect to seperator, '
+                    'starting from 0',
                     type=int)
 parser.add_argument('-i', '--typeint',
                     action='store_true',
                     help='treat the parsed word is int, assumed string by default')
-################################################## FINDING WORDS ENDS ##########################################################
-################################################## TREAT AS INT ##########################################################
+# FINDING WORDS ENDS
+# TREAT AS INT
 parser.add_argument('-a', '--base',
                     action='store',
                     help='base of the found word, eg: 16, default is 10',
@@ -74,149 +78,86 @@ parser.add_argument('-c', '--showifchanged',
 parser.add_argument('-w', '--drawgraph',
                     action='store_true',
                     help='draw graph and save it to the current directory with name \'output.png\'')
-################################################## TREAT AS INT ENDS ##########################################################
-# use action="store_true" for flags
-# type=int for max-min threshold
-
+# TREAT AS INT ENDS
 args = parser.parse_args()
 
-def debug_log(message):
-    if args.verbose == True:
-        print(message)
+egu = egrep_utils.Utils(args)
 
-if args.version == True:
+if args.version is True:
     print('version', VERSION)
     sys.exit(0)
 
-if args.filename == None:
-    print('filename cannot be None')
+if egu.arg_check() is False:
     sys.exit(1)
 
-if args.keywords == None:
-    print('keywords cannot be None')
-    sys.exit(1)
+egu.debug_log('filename: ' + args.filename)
+egu.debug_log('keywords: ' + args.keywords)
 
-debug_log('filename: ' + args.filename)
-debug_log('keywords: ' + args.keywords)
-
-line_number = 0
-total_found_line = 0
-line_below = 0
 final_line_no = 0
-last_found = 0
+line_number = 0
+line_below = 0
 x_coord = []
-height = []
+global_return = {}
 
-if args.linebelow != None:
+global_return["total_found_line"] = 0
+global_return["height"] = []
+
+if args.linebelow is not None:
     line_below = args.linebelow
 
- 
 # Opening file
 try:
-  with open(args.filename, 'r') as log_file_fp:
-    
-    # Using for loop
-    for line in log_file_fp:
-        line_number += 1
+    with open(args.filename, 'r') as log_file_fp:
+        # Using for loop
+        for line in log_file_fp:
+            line_number += 1
 
-        if args.endline != None and line_number > args.endline:
-            break
-        if  args.startline != None and line_number < args.startline:
-            continue
-
-        is_line_picked = True
-        keywords = (args.keywords).split(",")
-
-        for keyword in keywords:
-            lcl_keyword = keyword
-            lcl_line = line.strip()
-            if args.notkeysensitive == True:
-                lcl_keyword = lcl_keyword.lower()
-                lcl_line = lcl_line.lower()
-            if lcl_keyword not in lcl_line:
-                is_line_picked = False
+            if args.endline is not None and line_number > args.endline:
                 break
-        
+            if args.startline is not None and line_number < args.startline:
+                continue
 
-        if is_line_picked == True:
-            final_line_no = line_number + line_below
+            is_line_picked = True
+            keywords = (args.keywords).split(",")
 
-        if final_line_no == line_number:
-            # here, start to parse the line
-            seperator = " "
-            if args.seperator != None:
-                seperator = args.seperator
+            for keyword in keywords:
+                lcl_keyword = keyword
+                lcl_line = line.strip()
+                if args.notkeysensitive is True:
+                    lcl_keyword = lcl_keyword.lower()
+                    lcl_line = lcl_line.lower()
+                if lcl_keyword not in lcl_line:
+                    is_line_picked = False
+                    break
 
-            words = (line.strip()).split(seperator)
-            if args.elementat != None:
-                elementat = args.elementat
-                for word in words:
-                    if word == seperator or word.strip() == seperator or len(word.strip()) == 0:
-                        words.remove(word)
-                        continue
+            if is_line_picked is True:
+                final_line_no = line_number + line_below
 
-                if args.typeint == False:
-                    total_found_line = total_found_line + 1
-                    if args.showlineno == True:
-                        print(str(line_number) + ' : ' + words[elementat])
-                    else:
-                        print(words[elementat])
-                else:
-                    # treat it is "int"
-                    should_be_shown = True
-                    base = 10
-                    if args.base != None:
-                        base = args.base
-                    value = int(words[elementat], base)
-                    if args.maxthreshold != None:
-                        if value > args.maxthreshold:
-                            should_be_shown = False
-                    if args.minthreshold != None:
-                        if value < args.minthreshold:
-                            should_be_shown = False
-                    if args.showifchanged == True:
-                        if last_found == words[elementat]:
-                            should_be_shown = False
-                    
-                    last_found = words[elementat]
+            if final_line_no == line_number:
+                global_return = egu.parse_line(
+                    line.strip(), line_number,
+                    global_return["total_found_line"], global_return["height"])
 
-                    if should_be_shown == True:
-                        total_found_line = total_found_line + 1
-                        if args.drawgraph:
-                            height.append(value)
-
-                        if args.showlineno == True:
-                            print(str(line_number) + ' : ' + str(value))
-                        else:
-                            print(str(value))
-
-            else:
-                total_found_line = total_found_line + 1
-                if args.showlineno == True:
-                    print(str(line_number) + ' : ' + line.strip())
-                else:
-                    print(line.strip())
-
-    # Closing files
-    log_file_fp.close()
+        # Closing files
+        log_file_fp.close()
 except IOError:
     print("IOError error occured while opening the file '", args.filename, "'")
 
 if args.drawgraph:
     just_number = 0
-    for h in height:
+    for h in global_return["height"]:
         just_number = just_number + 1
         x_coord.append(just_number)
-    
+
     # plotting a bar chart
-    plt.bar(x_coord, height, color = ['grey'])
-    
+    plt.bar(x_coord, global_return["height"], color=['grey'])
+
     plt.xlabel('x - axis')
     plt.ylabel('y - axis')
     plt.title('output')
-    
+
     plt.savefig('output.png')
 
 print("############## SUMMARY ############")
 print("    total processed line   ", line_number)
-print("    total found line       ", total_found_line)
+print("    total found line       ", global_return["total_found_line"])
---
+++ run_flake8.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+flake8 --exclude .git,__pycache__ --max-line-length=120
+
